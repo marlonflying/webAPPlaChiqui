1bdf6b4d8d691b84a839cd96f88c1caf91e7854c
Marlon <marlonflying@gmail.com>
2020-04-17 20:36:26 -0500

    Handlers

--- main.go
+++ main.go
@@ -7,14 +7,36 @@ import (
 
 //home handler function with a string as the response
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Home response from La Chiqui webAPP"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		w.WriteHeader(405)
+		w.Write([]byte("Method Not Allowed"))
+		return
+	}
+
+	w.Write([]byte("Create a new snippet..."))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Starting server on :4000")
-	err := http.ListenAndServe("4000", mux)
+	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
